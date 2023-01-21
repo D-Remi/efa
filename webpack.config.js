909e0858efc7dbdd6a3efd1d4b9dee7f66f0d265
rdelfosse <remi.delfosse.ext@d2lgroup.com>
2023-01-21 19:19:15 +0100

    fix iniatialisation of react

--- webpack.config.js
+++ webpack.config.js
@@ -73,4 +73,10 @@ Encore
     //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();
+const config = Encore.getWebpackConfig();
+config.watchOptions = {
+    poll: true,
+};
+
+// export the final configuration
+module.exports = config;
